3c60b488147127c5c865ff3ecd47000868ca518e
Gabriel SULLICE <gabriel@sullice.com>
2019-06-12 16:32:06 -0600

    Revert "Remove ExplorerUI and integrate it back into App.js"
    This reverts commit 9f1b762088d55187dac8c6c7212d0c7288ffbfe2.

--- src/app.js
+++ src/app.js
@@ -1,43 +1,14 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 
-import {Location, LocationContext} from './contexts/location';
-import {LinkElement} from "./components/link";
-import Resource from "./components/resource";
+import { Location } from './contexts/location';
+import ExplorerUI from './components/explorer-ui';
 
 const homeUrl = process.env.TOP_LEVEL;
 
 const App = () => {
-  const { locationUrl, responseDocument, onEntryPoint } = useContext(
-    LocationContext,
-  );
-  const [entryPointLinks, setEntryPointLinks] = useState({});
-  const parsedLinks = responseDocument ? responseDocument.getLinks() : {};
-
-  useEffect(() => {
-    if (onEntryPoint) setEntryPointLinks(parsedLinks);
-  }, [onEntryPoint]);
-
   return (
     <Location homeUrl={homeUrl}>
-      <div className="container">
-        <header className="location">
-          <div className="pane query">
-            <h2>Location</h2>
-            <div className="scrollable scrollable_x query-url">{locationUrl}</div>
-          </div>
-        </header>
-        <nav className="pane resourceLinks">
-          <h2>Resources</h2>
-          <ul className="scrollable scrollable_y">
-            {Object.keys(entryPointLinks).map((key, index) => (
-              <li key={`resource-link-${index}`}>
-                <LinkElement link={entryPointLinks[key]} />
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <Resource links={!onEntryPoint ? parsedLinks : {}} />
-      </div>
+      <ExplorerUI />
     </Location>
   );
 };
