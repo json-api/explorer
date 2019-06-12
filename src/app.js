9f1b762088d55187dac8c6c7212d0c7288ffbfe2
Gabriel SULLICE <gabriel@sullice.com>
2019-06-12 16:18:34 -0600

    Remove ExplorerUI and integrate it back into App.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
-import { Location } from './contexts/location';
-import ExplorerUI from './components/explorer-ui';
+import {Location, LocationContext} from './contexts/location';
+import {LinkElement} from "./components/link";
+import Resource from "./components/resource";
 
 const homeUrl = process.env.TOP_LEVEL;
 
 const App = () => {
+  const { locationUrl, responseDocument, onEntryPoint } = useContext(
+    LocationContext,
+  );
+  const [entryPointLinks, setEntryPointLinks] = useState({});
+  const parsedLinks = responseDocument ? responseDocument.getLinks() : {};
+
+  useEffect(() => {
+    if (onEntryPoint) setEntryPointLinks(parsedLinks);
+  }, [onEntryPoint]);
+
   return (
     <Location homeUrl={homeUrl}>
-      <ExplorerUI />
+      <div className="container">
+        <header className="location">
+          <div className="pane query">
+            <h2>Location</h2>
+            <div className="scrollable scrollable_x query-url">{locationUrl}</div>
+          </div>
+        </header>
+        <nav className="pane resourceLinks">
+          <h2>Resources</h2>
+          <ul className="scrollable scrollable_y">
+            {Object.keys(entryPointLinks).map((key, index) => (
+              <li key={`resource-link-${index}`}>
+                <LinkElement link={entryPointLinks[key]} />
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Resource links={!onEntryPoint ? parsedLinks : {}} />
+      </div>
     </Location>
   );
 };
