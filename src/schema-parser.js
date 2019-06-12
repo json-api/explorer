52ec403de3a48aff7baf6fd4038717b8ca1b38cc
Gabriel SULLICE <gabriel@sullice.com>
2019-06-12 14:05:30 -0600

    Cache schema per resource regardless of filters

--- src/schema-parser.js
+++ src/schema-parser.js
@@ -25,13 +25,14 @@ export default class SchemaParser {
     const selfURL = extract(links, 'self.href');
     const parsedSelfURL = parseJsonApiUrl(selfURL);
     if (Object.keys(parsedSelfURL.query.fields).length) {
-      parsedSelfURL.query.fields = [];
+      const completeFieldsetUrl = Object.assign({}, parsedSelfURL, {query: Object.assign({}, parsedSelfURL.query, {fields: []})});
       return this.parse(
-        Document.parse(await request(compileJsonApiUrl(parsedSelfURL))),
+        Document.parse(await request(compileJsonApiUrl(completeFieldsetUrl))),
         forPath,
       );
     }
-    return this.inferSchema(root, forPath);
+    const baseResourceURL = compileJsonApiUrl(Object.assign({}, parsedSelfURL, {protocol: 'inferred:', query: {}}));
+    return this.mergeWithCachedInference(baseResourceURL, this.inferSchema(root, forPath));
   }
 
   parseSchema(schema, forPath) {
