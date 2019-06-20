ce0be7c1fc66b448b6b108392352e441a6f5851c
zrpnr <peter.weber@acquia.com>
2019-06-20 13:17:43 -0600

    Recurse menu-attribute to display objects

--- src/components/schema-ui/schema-menu-attribute.js
+++ src/components/schema-ui/schema-menu-attribute.js
@@ -1,32 +1,52 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+const SchemaMenuAttributeName = ({ name }) => (
+  <div className="menu__attribute">
+    <span className="link__title link__title--readable">{name}</span>
+  </div>
+);
+
+const SchemaMenuAttributeValue = ({ name, value }) => {
+  const { type, title, description, properties, ...values } = value;
+
+  return type === 'object' ? (
+    <div className="menu__attribute menu__attribute_object">
+      <span className="link__title link__title--readable">{title}</span>
+      <span className="link__text link__text--machine">{name}</span>
+      <ul className="menu__attribute_properties">
+        {Object.entries(properties).map(([key, value], index) => (
+          <li key={`${name}-${key}-${index}`}>
+            <SchemaMenuAttribute attribute={{ name: key,value }} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <div className="menu__attribute">
+      <span className="link__title link__title--readable">{title}</span>
+      <span className="link__text link__text--machine">
+        {name}:<span className="link__text--type">{type}</span>
+      </span>
+      <ul className="menu__attribute_properties">
+        {Object.entries(values).map(([key, value], index) => (
+          <li key={`${name}-${key}-${index}`}>
+            <span className="link__text link__text--machine">
+              {key}: <strong>{typeof value === 'object' ? JSON.stringify(value) : value}</strong>
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const SchemaMenuAttribute = ({ attribute }) => {
-  const [showValue, setShowValue] = useState(false);
+  const { name, value } = attribute;
 
-  return (
-    <span className="menu__attribute">
-      <span
-        className="link__title link__title--readable"
-        onClick={() => setShowValue(!showValue)}
-      >
-        {attribute.name}
-      </span>
-      {attribute.value && (
-        <ul
-          className={`menu__attribute_properties ${
-            showValue ? 'is-active' : 'is-inactive'
-          }`}
-        >
-          {Object.entries(attribute.value).map(([key, value], index) => (
-            <li key={`${attribute.name}-${key}-${index}`}>
-              <span className="link__text link__text--machine">
-                {key}: <strong>{JSON.stringify(value)}</strong>
-              </span>
-            </li>
-          ))}
-        </ul>
-      )}
-    </span>
+  return value ? (
+    <SchemaMenuAttributeValue name={name} value={value} />
+  ) : (
+    <SchemaMenuAttributeName name={name} />
   );
 };
 
