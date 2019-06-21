98d4dcd5fb8bd6f9e2a7e37789e0458f9a01b778
zrpnr <peter.weber@acquia.com>
2019-06-21 11:59:38 -0600

    Load multiple levels of relationships

--- src/components/param-ui/include-loader.js
+++ src/components/param-ui/include-loader.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import useSchema from '../../hooks/use-schema';
 
-const IncludeLoader = forPath => {
-
+const IncludeLoaderList = ({forPath, load}) => {
   const schema = useSchema(forPath);
 
   if (schema) {
     const { relationships } = schema;
-    return (
-      relationships.map(relationship => <div>{relationship.name}</div>)
-    );
+    return relationships.map((relationship, index) => (
+      <div key={index}
+        onClick={() => load({
+            title: relationship.name,
+            forPath: [...forPath, relationship.name],
+          })
+        }
+      >
+        {relationship.name}
+      </div>
+    ));
   }
-  return (<div></div>)
+  return <div />;
+};
 
+const IncludeLoader = () => {
+  const [loadedInclude, setLoadedInclude] = useState([]);
+  const schema = useSchema([]);
+
+  const loadNext = next => {
+    const loaded = loadedInclude.slice(0, next.forPath.length);
+    setLoadedInclude([...loaded, next]);
+  };
+
+  useEffect(() => {
+    if (schema) {
+      loadNext({title: schema.type, forPath: []});
+    }
+  }, [schema]);
+
+  return loadedInclude.map((include, index) => (
+    <IncludeLoaderList key={index} forPath={include.forPath} load={loadNext} />
+  ));
 };
 
 export default IncludeLoader;
