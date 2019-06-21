d7331a01acd267dc09f9e7a423001ec163ac4f9f
Gabriel SULLICE <gabriel@sullice.com>
2019-06-21 11:18:24 -0600

    add focus animation

--- src/components/result-ui/summary.js
+++ src/components/result-ui/summary.js
@@ -21,18 +21,18 @@ const FieldValue = ({value}) => {
 
 const FieldRow = ({ fieldPath, fieldValue }) => {
   return (
-    <>
-      <div className="result-row-path">{fieldPath}</div>
-      <div className="result-row-value"> {
+    <div>
+      <div className="results__field__path">{fieldPath}</div>
+      <div className="results__field__value"> {
         Array.isArray(fieldValue)
           ? (
             fieldValue.length ? <ul>
               {fieldValue.map((value, i) => (<li key={i}><FieldValue value={value} /></li>))}
-            </ul> : <span className="empty-array">empty</span>
+            </ul> : <span className="results__field__value--empty">empty</span>
           )
           : <FieldValue value={fieldValue} />
       }</div>
-    </>
+    </div>
   );
 };
 
@@ -46,27 +46,36 @@ const Summary = ({data}) => {
     }
   }, [focusPath]);
 
-  const resourceObjects = [data]
-    .flat()
-    .filter(obj => !zoom || obj.matches(zoom));
+  const resourceObjects = [data].flat();
 
   return (resourceObjects.length && <div className="results">
     <ul>
       {resourceObjects.map((resourceObject, i) => {
         const type = resourceObject.getType(), id = resourceObject.getID();
-        const attributes = Object.entries(resourceObject.getAttributes()).filter(([fieldPath]) => !focusPath || fieldPath === focusPath);
+        const attributes = Object.entries(resourceObject.getAttributes());
         const links = Object.entries(resourceObject.getOutgoingLinks());
         const showExpand = focusPath && !zoom;
+        const isZoomed = zoom && resourceObject.matches(zoom);
+        const rowClass = ['results__row'].concat(zoom && !isZoomed ? ['results__row--hidden'] : []);
+        let above = true;
         return (
-          <li key={`result-row-${i}`} className="result-row" title={`${id} (${type})`}>
+          <li key={`result-row-${i}`} className={rowClass.join(' ')} title={`${id} (${type})`}>
             <ul>
-            {attributes.map(([fieldPath, fieldValue], j) => (
-              <li key={`result-row-${i}-field-${j}`} className="result-row-field">
-                <FieldRow fieldPath={fieldPath} fieldValue={fieldValue}/>
-              </li>
-            ))}
+            {attributes.map(([fieldPath, fieldValue], j) => {
+              const isFocused = !focusPath || fieldPath === focusPath;
+              if (isFocused) {
+                above = false;
+              }
+              let fieldClass = 'results__field';
+              fieldClass += focusPath && !isFocused ? ` results__field--hidden results__field--hidden-${above ? 'above' : 'below'}` : '';
+              return (
+                <li key={`result-row-${i}-field-${j}`} className={fieldClass}>
+                  <FieldRow fieldPath={fieldPath} fieldValue={fieldValue}/>
+                </li>
+              );
+            })}
             </ul>
-            <div className="result-row-actions">
+            <div className="results__actions">
               <ul>
                 {showExpand && <li key={'zoom'}><button onClick={() => {
                   setZoom({type, id});
