9675a542402b3a083d84afdad48b084bafb1db63
Gabriel SULLICE <gabriel@sullice.com>
2019-06-28 14:07:21 -0600

    Adds pagination buttons and a cleaner result count

--- src/components/result-ui/display-raw.js
+++ src/components/result-ui/display-raw.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 import CodeMirrorElem from './code-mirror';
+import {LocationContext} from "../../contexts/location";
+
+const PageNavigation = ({links}) => {
+  const location = useContext(LocationContext);
+  const {first, prev, next, last} = links;
+
+  const followLink = link => () => {
+    location.setUrl(link.href);
+  };
+
+  return (<div className="page_navigation">
+    {first && <span className="page_navigation__link_arrow" onClick={followLink(first)}>&laquo;</span>}
+    {prev && <span className="page_navigation__link_arrow" onClick={followLink(prev)}>&lsaquo;</span>}
+    {next && <span className="page_navigation__link_arrow" onClick={followLink(next)}>&rsaquo;</span>}
+    {last && <span className="page_navigation__link_arrow" onClick={followLink(last)}>&raquo;</span>}
+  </div>);
+};
 
 const DisplayRaw = ({ title, name, responseDocument, children }) => {
   const [activeTab, setActiveTab] = useState(0);
+  const isCollection = responseDocument && responseDocument.isCollectionDocument();
+  const resultCount = isCollection ? responseDocument.getData().length : 1;
+  const links = responseDocument ? responseDocument.getPaginationLinks() : false;
 
   const TabMenu = ({ title, id }) => (
     <li
@@ -23,16 +43,19 @@ const DisplayRaw = ({ title, name, responseDocument, children }) => {
             <TabMenu id={1} title="Raw" />
           </ul>
           <div className={`tab flex-height ${activeTab === 0 ? 'tab__active' : ''}`}>
-            <h2 className="tab__title">{title}</h2>
-            {(!Array.isArray(responseDocument.data) || responseDocument.data.length)
-              ? children
-              : <div />
-            }
+            <div className="tab__header">
+              <span className="tab__header__result_count">{isCollection ? resultCount ? `${resultCount} results` : 'No results' : 'Result'}</span>
+              {links && <PageNavigation links={links}/>}
+            </div>
+            {resultCount ? children : <></>}
           </div>
           <div className={`tab flex-height ${activeTab === 1 ? 'tab__active' : ''}`}>
-            <h2 className="tab__title">Raw</h2>
+            <div className="tab__header">
+              <span className="tab__header__result_count">{isCollection ? resultCount ? `${resultCount} results` : 'No results' : 'Result'}</span>
+              {links && <PageNavigation links={links}/>}
+            </div>
             <CodeMirrorElem
-              code={JSON.stringify(responseDocument, null, '  ')}
+              code={JSON.stringify(responseDocument.raw, null, '  ')}
             />
           </div>
         </>
