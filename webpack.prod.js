7fa1ef1f13cdc83644f31ba4f97959faf5ff4277
zrpnr <peter.weber@acquia.com>
2019-07-02 11:44:14 -0600

    Remove babel minify and unused config file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
+// const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
-  plugins: [
-    new MinifyPlugin({
-      "evaluate": false,
-      "mangle": true,
-      "deadcode": true
-    })
-  ]
+  mode: 'production'
 });
