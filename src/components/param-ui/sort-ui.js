76df73c140d686155e26971c0fb1e27010c8b053
zrpnr <peter.weber@acquia.com>
2019-07-02 15:47:40 -0600

    Display current sort

--- src/components/param-ui/sort-ui.js
+++ src/components/param-ui/sort-ui.js
@@ -6,39 +6,43 @@ import { Close } from '../icon';
 
 import { LocationContext } from '../../contexts/location';
 
-const SortUI = () => {
-  const { sort, setSort } = useContext(LocationContext);
+const directions = {
+  ASC: 'Ascending',
+  DESC: 'Descending',
+};
+
+const SortActive = ({ name, direction }) => {
+  const { setSort } = useContext(LocationContext);
 
   const removeSort = name => {
-    
-  }
+    setSort([...sort.filter(param => name !== name)]);
+  };
+
+  return (
+    <div className="param_ui__item param_ui__item--large param_ui__item--sort">
+      <span>
+        <code>{name}</code>
+        <abbr title={directions[direction]}>{direction}</abbr>
+      </span>
+      <button className="param_ui__button--icon" onClick={removeSort}>
+        <Close />
+      </button>
+    </div>
+  );
+};
+
+const SortUI = () => {
+  const { sort } = useContext(LocationContext);
 
   return (
     <ParamUI name="sort" title="Sort">
       <SortLoader />
       {sort.map((param, index) => (
-        <div
-          key={`${param.name}-${index}`}
-          className="param_ui__item param_ui__item--large param_ui__item--sort"
-        >
-          <span>
-            <code>{param.name}</code>
-            <abbr
-              title={`${
-                param.direction === 'ASC' ? 'Ascending' : 'Descending'
-              }`}
-            >
-              {param.direction}
-            </abbr>
-          </span>
-          <button
-            key={`${fObj.id}-${index}`}
-            className="param_ui__button--icon"
-            onClick={() => setFilter(fObj.id, 'delete')}
-          >
-            <Close />
-          </button>
-        </div>
+        <SortActive
+          key={`${param.path}-${index}`}
+          name={param.path}
+          direction={param.direction}
+        />
       ))}
     </ParamUI>
   );
