bb577c10dbb384efd369ab45af294a3ac8df24dc
zrpnr <peter.weber@acquia.com>
2019-07-10 14:19:52 -0600

    Display example location suggestion

--- src/components/location-ui/index.js
+++ src/components/location-ui/index.js
@@ -5,6 +5,11 @@ const LocationBar = ({ onNewUrl, value = '' }) => {
 
   useEffect(() => setInputUrl(value), [value]);
 
+  const sampleUrl = 'https://example.jsonapi.dev';
+  const setSampleLocation = () => {
+    onNewUrl(sampleUrl);
+  };
+
   const handleSubmit = e => {
     e.preventDefault();
     inputUrl.length && onNewUrl(inputUrl);
@@ -15,10 +20,25 @@ const LocationBar = ({ onNewUrl, value = '' }) => {
       <input
         type="url"
         className="query-url"
-        placeholder="Enter a JSON:API server URL to begin exploring... e.g. https://example.com/api"
+        placeholder="Enter a JSON:API server URL to begin exploring"
         value={inputUrl}
         onChange={e => setInputUrl(e.target.value)}
       />
+      <div className="location__suggestion">
+        <div
+          className={`location__suggestion_text location__suggestion_text--${
+            inputUrl === '' ? 'active' : 'hidden'
+          }`}
+        >
+          <span>...or try</span>
+          <button
+            className="location__suggestion_button"
+            onClick={setSampleLocation}
+          >
+            <code>{sampleUrl}</code>
+          </button>
+        </div>
+      </div>
     </form>
   );
 };
