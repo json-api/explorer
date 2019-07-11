0e9aa6a3baea0c54d4499cc1db944834ae5ba01f
zrpnr <peter.weber@acquia.com>
2019-07-10 21:24:37 -0600

    Update example button style per feedback

--- src/components/location-ui/index.js
+++ src/components/location-ui/index.js
@@ -17,31 +17,31 @@ const LocationBar = ({ onNewUrl, value = '' }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="location">
-      <input
-        type="url"
-        className="query-url"
-        placeholder="Enter a JSON:API server URL to begin exploring"
-        value={inputUrl}
-        onChange={e => setInputUrl(e.target.value)}
-      />
-      <div className="location__suggestion">
-        <div
-          className={`location__suggestion_text location__suggestion_text--${
-            inputUrl === '' ? 'active' : 'hidden'
-          }`}
+    <div className="location">
+      <form className="location__form" onSubmit={handleSubmit}>
+        <input
+          type="url"
+          className="query-url"
+          placeholder="Enter a JSON:API server URL to begin exploring"
+          value={inputUrl}
+          onChange={e => setInputUrl(e.target.value)}
+        />
+      </form>
+      <div
+        className={`location__suggestion location__suggestion--${
+          inputUrl === '' ? 'active' : 'hidden'
+        }`}
+      >
+        <span>or try an </span>
+        <button
+          className="location__suggestion_button"
+          onClick={setSampleLocation}
+          disabled={inputUrl !== ''}
         >
-          <span>...or try</span>
-          <a
-            href="#"
-            className="location__suggestion_link"
-            onClick={setSampleLocation}
-          >
-            <code>{sampleUrl}</code>
-          </a>
-        </div>
+          example
+        </button>
       </div>
-    </form>
+    </div>
   );
 };
 
